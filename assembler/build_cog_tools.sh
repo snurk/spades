4865aad17ca751d43ef01edae09ac53c6bd42f38
Sergey Nurk <sergeynurk@gmail.com>
2020-01-25 13:45:19 -0500

    add spades compilation to build script

--- assembler/build_cog_tools.sh
+++ assembler/build_cog_tools.sh
@@ -2,6 +2,7 @@
 set -e
 
 ./prepare_cfg
+./spades_compile.sh
 make -C build/release/projects/unitig_coverage/ -j8
 make -C build/release/projects/gsimplifier/ -j8
 make -C build/release/projects/cds_subgraphs/ -j8
